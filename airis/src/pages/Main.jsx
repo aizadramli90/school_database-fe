a48cb172862564ae2116e6a0d89e917a0c540a24
aizadramli90 <muhammadaizadr@gmail.com>
2024-09-04 23:16:34 +0800

    add carousel not func

---
+++ airis/src/pages/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Main = () => {
+  return (
+    <div>
+      <nav>
+        
+      </nav>
+    </div>
+  )
+}
+
+Main.propTypes = {
+
+}
+
+export default Main
